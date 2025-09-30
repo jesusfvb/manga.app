21de454faabcfeb66dcf0d9d37f756c2050451de
Jesús Francisco Vázquez Biltre <jfvazquezbiltre@gmail.com>
2025-09-30 17:18:54 +0200

    feat(deps): Add dependencies for support svg

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,22 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-const { withNativeWind } = require("nativewind/metro");
+const { withNativeWind } = require('nativewind/metro');
 
+const defaultConfig = getDefaultConfig(__dirname);
 
-const myConfig = {};
+const { assetExts, sourceExts } = defaultConfig.resolver;
 
-const config = mergeConfig(getDefaultConfig(__dirname), myConfig);
+const myConfig = {
+  transformer: {
+    babelTransformerPath: require.resolve(
+      'react-native-svg-transformer/react-native',
+    ),
+  },
+  resolver: {
+    assetExts: assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+  },
+};
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+const config = mergeConfig(defaultConfig, myConfig);
+
+module.exports = withNativeWind(config, { input: './global.css' });
