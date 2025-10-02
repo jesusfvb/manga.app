07a2a5ca90f9eafb3d5e4508d8e82232f99500a4
Jesús Francisco Vázquez Biltre <jfvazquezbiltre@gmail.com>
2025-10-02 21:42:20 +0200

    feat(assets): Added a new placeholder image to the Images export in src/assets/index.ts
    This allows for easier referencing of a default image throughout the
    app.
    No changes were made to the SVG icons export

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,8 +2,10 @@ import IconTorii from './icons/icon_torii.svg';
 
 export const Images = {
   iconChopper: require('./icons/icon_chopper.jpg'),
+  placeholder: require('./images/placeholder.jpg'),
 };
 
 export const IconsSVG = {
   iconTorii: IconTorii,
 };
+
